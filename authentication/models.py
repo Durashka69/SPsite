8885b20a7f3f3fb92576ee8a5673b3090b7c7f71
kiri3914 <you@example.com>
2021-11-28 00:14:40 +0600

    хули там

--- authentication/models.py
+++ authentication/models.py
@@ -6,9 +6,10 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=255, blank=False)
     last_name = models.CharField(max_length=255, blank=False)
     age = models.PositiveIntegerField(default=16)
-    email = models.EmailField(verbose_name='Почта',max_length=255,blank=False)
+    email = models.EmailField(verbose_name='Почта', max_length=255, blank=False)
     image = models.ImageField(upload_to='user/profile')
     position = models.CharField(max_length=255, blank=False)
+    telegram = models.CharField(max_length=255, blank=True)
 
     def __str__(self):
         return self.first_name, self.last_name
