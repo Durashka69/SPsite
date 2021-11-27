979d15993bb2588f350dfac1ff2ebc0d14bacbc0
Glen <gulyashshsh@gmail.com>
2021-11-28 00:56:35 +0600

    just commit

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
