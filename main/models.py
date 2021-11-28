7e4f96fe29e6ea68a325ee804d077d7947ab3250
kiri3914 <you@example.com>
2021-11-28 22:34:18 +0600

    V1

--- main/models.py
+++ main/models.py
@@ -1,12 +1,50 @@
 from django.db import models
 
 
+class Position(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название должности')
+    slug = models.SlugField(verbose_name='Слаг', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+
+
 class Project(models.Model):
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='media/project')
-    description = models.TextField()
-    customer = models.CharField(max_length=255)
-    date = models.DateField(auto_now_add=False)
-    link = models.CharField(max_length=300, blank=True)
+    title = models.CharField(max_length=255, verbose_name="Название Проекта")
+    image = models.ImageField(upload_to='media/project', verbose_name="Изображение")
+    description = models.TextField(verbose_name="Описание")
+    customer = models.CharField(max_length=255, verbose_name='Заказчик')
+    date = models.DateField(auto_now_add=False, verbose_name="Дата сдачи пректа")
+    link = models.CharField(max_length=300, blank=True, verbose_name="Ссылка на проект")
+    slug = models.SlugField(verbose_name='Слаг', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
 
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=False)
+    date_joined = models.DateField(verbose_name='С каких пор работает', auto_now_add=False)
+    description = models.TextField(verbose_name='Описание')
+    position = models.ForeignKey(Position, verbose_name='Должность', on_delete=models.CASCADE)
+    image = models.ImageField(verbose_name='Фото', upload_to='media/persons')
+    age = models.PositiveIntegerField(default=16, verbose_name='Возраст')
+    email = models.EmailField(verbose_name='Почта', max_length=255, blank=False)
+    telegram = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(verbose_name='Слаг', unique=True)
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
--- main/views.py
+++ main/views.py
@@ -3,11 +3,41 @@ from .serializers import *
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
+class PositionListCreateView(ListCreateAPIView):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializers
+
+
+class PositionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializers
+
+
 class ProjectListCreateView(ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializers
 
 
-class ProejctRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+class ProjectRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializers
+
+
+class PersonInListCreateView(ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonInSerializers
+
+
+class PersonInRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonInSerializers
+
+
+class PersonOutListCreateView(ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonOutSerializers
+
+
+class PersonOutRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonOutSerializers
